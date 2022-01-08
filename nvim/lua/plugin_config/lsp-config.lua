5a2fb56af52df86164dbebdbb15a7e9e058b901d
Kai Kosades <red4truck@tutanota.com>
2022-01-08 17:16:35 +0200

    Removed temp files, changed configs Improved interface between tmux and neovim

--- nvim/lua/plugin_config/lsp-config.lua
+++ nvim/lua/plugin_config/lsp-config.lua
@@ -1,5 +1,3 @@
-local inspect = require("plugin_config/inspect")
-
 -- Installer
 local lsp_installer = require("nvim-lsp-installer")
 
@@ -7,14 +5,9 @@ local lsp_installer = require("nvim-lsp-installer")
 lsp_installer.on_server_ready(function(server)
 
 	server:setup({})
-	vim.cmd [[ do User LspAttachBuffers ]]
+	--vim.cmd [[ do User LspAttachBuffers ]]
 
 	-- Starting COQ
 	server:setup(coq.lsp_ensure_capabilities())
 	vim.cmd [[ COQnow -s ]]
 end)
-
-
---lsp_config["sumneko_lua"].setup({
-	--cmd={"sumneko_lua"}
---})
