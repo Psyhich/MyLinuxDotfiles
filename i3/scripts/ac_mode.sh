6db5b81284a3185295029cdb2c598c7fb1b90f96
Kai Kosades <red4truck@tutanota.com>
2022-01-16 03:35:11 +0200

    Removed redundant files and lines from configs

--- i3/scripts/ac_mode.sh
+++ i3/scripts/ac_mode.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
-TEMPS="/home/psyhich/.config/i3/temps"
 
-# kill -s kill "$(cat $TEMPS/xwinwrap_pid)" > /dev/null 2>&1 
-kill -s kill "$(cat $TEMPS/feh_pid)" > /dev/null 2>&1
-kill -s kill "$(cat $TEMPS/picom_pid)" > /dev/null 2>&1
+TEMPS="/home/psyhich/.config/i3/temps"
 
 sleep 0.5
 
+# For transparency
+kill -s kill "$(cat $TEMPS/picom_pid)" > /dev/null 2>&1
 picom --experimental-backends --backend glx --xrender-sync-fence &
 echo "$!" > "$TEMPS/picom_pid"
 
-feh --bg-scale ~/Pictures/Wallpapers/current_wallpaper &
+# Setting background
+feh --bg-fill ~/Pictures/Wallpapers/current_wallpaper &
 cat "$!" > "$TEMPS/feh_pid"
 
+# For animated wallpapers
+# kill -s kill "$(cat $TEMPS/xwinwrap_pid)" > /dev/null 2>&1 
 # xwinwrap -g 1920x1080 -ni -s -nf -b -un -ov -fdt -argb -- mpv --vo=gpu --hwdec=vdpau --mute=yes --no-audio --no-osc --no-osd-bar --quiet --screen=0 --geometry=1920x1080+0+0 -wid WID --loop /home/psyhich/Pictures/AnimatedWallpaper/CurrentWallpaper.mp4 >> /home/psyhich/logs.log 2>&1 &
 # echo "$!" > "$TEMPS/xwinwrap_pid"
 
--- i3/scripts/battery_mode.sh
+++ i3/scripts/battery_mode.sh
@@ -2,9 +2,8 @@
 TEMPS="/home/psyhich/.config/i3/temps"
 
 # kill -s kill "$(cat $TEMPS/xwinwrap_pid)" > /dev/null 2>&1 
-kill -s kill "$(cat $TEMPS/feh_pid)" > /dev/null 2>&1
 kill -s kill "$(cat $TEMPS/picom_pid)" > /dev/null 2>&1
 
-feh --bg-scale ~/Pictures/Wallpapers/current_wallpaper &
+feh --bg-fill ~/Pictures/Wallpapers/current_wallpaper &
 cat "$!" > "$TEMPS/feh_pid"
 
